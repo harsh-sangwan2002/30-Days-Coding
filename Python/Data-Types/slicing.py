c51a8db72d7dac62ca119b2d9d5abfdb408d4850
Harsh Sangwan <hsangwan2002@gmail.com>
2025-02-20 21:10:00 +0530

    completed data types and operators

---
+++ Python/Data-Types/slicing.py
@@ -0,0 +1,4 @@
+my_string = "Hello! World"
+print(my_string[0:5])
+print(my_string[:2])
+print(my_string[::2])
---
+++ Python/Operators/logical.py
@@ -0,0 +1,5 @@
+x = 5; y = 10; z = 5
+
+print((x<y) and (x==z))
+print((x<y) or (x==z))
+print(not(x<y))
